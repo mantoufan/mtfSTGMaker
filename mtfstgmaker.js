1d80e493bdc18bf50dd7a64be8afc5211dbc96bf
馒头饭 <mhjlw@126.com>
2020-11-22 22:03:06 +0800

    :new:增加对子类对父类原型上的方法继承函数

--- mtfstgmaker.js
+++ mtfstgmaker.js
@@ -9,41 +9,49 @@
  */
 var mtfSTGMaker = (function() {
     /**
-     * 原型：可移动对象
+     * 函数：子类仅继承父类原型链上的方法，构造函数正确
+     * @param {Constructor} subType 
+     * @param {Constructor} superType 
      */
-    function MovableObject(x, y) {
-        this.x = x;
-        this.y = y;
+    function inherit(subType, superType) {
+        var prototype = Object.create(superType.prototype)
+            prototype.constructor = subType
+            subType.prototype = prototype
     }
     /**
-     * 移动
-     * @param {String<left|right|up|down>} direction 移动方向 
-     * @param {Number} step 移动速度
+     * 原型：可移动对象
+     * @param {Object} opt 对象属性
+     * @param {Integer} opt.x 横坐标
+     * @param {Integer} opt.y 纵坐标
+     * @param {Integer} opt.size 大小
+     * @param {Integer} opt.speed 速度
      */
-    MovableObject.prototype.move = function(direction, step) {
-        switch (direction) {
-            case 'left':
-                this.x -= step;
-                break;
-            case 'right':
-                this.x += step;
-                break;
-            case 'up':
-                this.y -= step;
-                break;
-            case 'down':
-                this.y += step;
-                break;
-            default:
-                break;
-        }
+    function MovableObject(opt) {
+        this.x = opt.x || 0
+        this.y = opt.y || 0
+        this.size = opt.size || 0
+        this.speed = opt.speed || 0
     }
-
+    /**
+     * 方法：移动
+     * @param {Integer} x 水平偏移量
+     * @param {Integer} y 竖直偏移量
+     */
+    MovableObject.prototype.move = function(offsetX, offsetY) {
+        this.x += offsetX
+        this.y += offsetY
+    }
     /**
      * 原型：可射击对象
+     * @param {Integer} x 水平偏移量
+     * @param {Integer} y 竖直偏移量
      */
     function ShootingObject(x, y) {
         MovableObject.call(this, x, y)
+    }
+    inherit(ShootingObject, MovableObject)
+    ShootingObject.prototype.shoot = function() {
+
     }
     return {
 
